d6d7500258b191e7024b11ea85a9ba5d957292a4
Lorenzo <lorenzo-a@live.fr>
2023-11-07 15:13:19 +0100

    Add neighbour

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import './style.css';
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/france');
-request.send();
-
-request.addEventListener('load', function() {
-    const [country] = JSON.parse(this.responseText);
+const renderCountry = (country, className="") => {
     const countryCard = `
-        <article class="country">
+        <article class="country ${className}">
             <img class="country__img" src="${country.flag}" />
             <div class="country__data">
             <h3 class="country__name">${country.name}</h3>
@@ -20,7 +15,35 @@ request.addEventListener('load', function() {
             <p class="country__row"><span>💰 ${country.currencies[0].name}</span>CUR</p>
             </div>
         </article>
-    `;
+    `
     countriesContainer.insertAdjacentHTML('beforeend', countryCard);
     countriesContainer.style.opacity = 1;
-})
+}
+
+const getCountryData = (country) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v2/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function() {
+        const [country] = JSON.parse(this.responseText);
+        renderCountry(country);
+
+        const [paysVoisin] = country.borders;
+        if(!paysVoisin) return;
+
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v2/alpha/${paysVoisin}`);
+        request2.send();
+
+        request2.addEventListener('load', function() {
+            const country2 = JSON.parse(this.responseText);
+            renderCountry(country2, 'neighbour');
+        })
+    })
+}
+
+// btn.addEventListener('click', () => {
+    getCountryData('france');
+// })
+
